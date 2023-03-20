f91fee63010bc2c23e637f06792468343bdb2845
Dexios102 <119906753+Dexios102@users.noreply.github.com>
2023-03-20 09:59:14 +0800

    loginCss

---
+++ app/Http/Controllers/LoginRegisterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginRegisterController extends Controller
+{
+    public function login() {
+        return view("signIn");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginRegisterController::class,"login"])->name('login');
