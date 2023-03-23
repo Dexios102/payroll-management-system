fa70988de192f9275c84069a4cbc460c870558b9
Dexios102 <119906753+Dexios102@users.noreply.github.com>
2023-03-23 09:07:40 +0800

    v

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,3 +1,11 @@
-<header>
-    <h3 class="logo">PAYROLL</h3>
-</header>
+<head>
+  <link rel="stylesheet" href="css/header.css"/>
+</head>
+<nav>
+  <div class="logo">
+    <a href="/dashboard"><img src="images/logo.png" alt="Logo"></a>
+  </div>
+  <ul class="nav-links">
+    <li><a href="#">About</a></li>
+  </ul>
+</nav>
