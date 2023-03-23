69126bb70e810632615c33e7e7036d83fe9c5014
Dexios102 <119906753+Dexios102@users.noreply.github.com>
2023-03-23 08:06:16 +0800

    update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,9 +1,3 @@
 <header>
     <h3 class="logo">PAYROLL</h3>
-    <nav>
-        <ul>
-          
-          <li><a href="#">Contactss</a></li>
-        </ul>
-      </nav>
 </header>
--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -49,27 +49,5 @@
                 Don't have an account? <a href="/signup">Create an account.</a>
             </div>
         </div>
-        <!-- <div class="login-card-social">
-            <div>Other Sign-In Options</div>
-            <div class="login-card-social-btns">
-                <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
-                    </svg>
-                </a>
-                <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-google" width="24"
-                        height="24" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8"></path>
-                    </svg>
-                </a>
-            </div>
-        </div>
-    </div> -->
 </body>
 </html>
